bfc468c31a4d028cc6521c29dd65c11f74e98f25
R0Bill <157518506+R0Bill@users.noreply.github.com>
2025-02-01 23:14:38 +0000

    test preload api

--- preload.js
+++ preload.js
@@ -5,3 +5,7 @@ contextBridge.exposeInMainWorld('versions', {
   chrome: () => process.versions.chrome,
   electron: () => process.versions.electron
 })
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  getNumber: () => 114514,
+})
--- renderer.js
+++ renderer.js
@@ -1,2 +1,4 @@
 const information = document.getElementById('info')
 information.innerText = `This app is using Chrome (v${window.versions.chrome()}), Node.js (v${window.versions.node()}), and Electron (v${window.versions.electron()})`
+const number = document.getElementById('fuckingTestNumber')
+number.innerText = `The number is ${window.electronAPI.getNumber()}`
