5062d8cb303b88a38fb10a3f6e4b0daf8adec7f8
R0Bill <157518506+R0Bill@users.noreply.github.com>
2025-02-03 22:49:37 +0000

    add a test function to locate which element is clicked

--- renderer.js
+++ renderer.js
@@ -1,6 +1,5 @@
 // Wait until the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("111");
     const articlesList = document.getElementById("articles");
     const articleDisplay = document.getElementById("article-content");
 
@@ -8,9 +7,11 @@ document.addEventListener("DOMContentLoaded", function () {
     articlesList.addEventListener("click", function (event) {
         const target = event.target;
         if (target && target.tagName === "LI") {
-            // Get content from data attribute and display it
-            const content = target.getAttribute("data-content");
-            articleDisplay.innerHTML = `<p>${content}</p>`;
+            // 获取点击的文章序号
+            const items = Array.from(target.parentNode.children);
+            const index = items.indexOf(target);
+            articleDisplay.innerHTML = `<p>you are clicking ${index} element</p>`;
         }
     });
 });
+
