f9d707bb494545f8aad585a59d7b946561e9220b
Tyler913 <hongyf050517@outlook.com>
2025-02-17 20:51:57 -0500

    change the forge config file

--- forge.config.js
+++ forge.config.js
@@ -3,21 +3,12 @@ module.exports = {
     makers: [
         {
             name: "@electron-forge/maker-zip",
-            platforms: ["win32"],
+            platforms: ["win32", "darwin"], // Now includes macOS
             config: {
                 artifactName: (forgeConfig, makerOptions, platform, arch) => {
                     return `${forgeConfig.packageJSON.name}-${forgeConfig.packageJSON.version}-${platform}-${arch}.zip`;
                 },
             },
-        },
-        {
-            name: "@electron-forge/maker-dmg",
-            platforms: ["darwin"],
-            config: {
-                artifactName: (forgeConfig, makerOptions, platform, arch) => {
-                    return `${forgeConfig.packageJSON.name}-${forgeConfig.packageJSON.version}-${platform}-${arch}.dmg`;
-                },
-            },
-        },
+        }
     ],
 };
